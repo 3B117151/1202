91e584a4287dc5b34d192e49a43ef7391cf5bdd8
3B117151 <114204306+3B117151@users.noreply.github.com>
2022-12-09 11:50:31 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,16 +1,20 @@
 public class Test {
 	public static void main (String[] args){
-		int [] iArr = {7,3,4,9,6};
-	
-		for (int x=0;x<iArr.length;x++){
-			if(iArr[x]>iArr[x+1]){
-			iArr[x]=iArr[x+1];
-			iArr[x+1]=iArr[x]
-			}
-		}
+			int [] iArr = {7,3,4,9,6};
 		
-		for (int x=0;x<iArr.length;x++){
-		System.out.println(">>"+iArr[x]);
-		}
-	}
+			for (int x=0;x<iArr.length;x++){
+				for(int y=0;y<iArr.length;y++){
+					if(iArr[x]>iArr[y]){
+						int temp=iArr[x];
+						iArr[x]=iArr[y];
+						iArr[y]=temp;
+						}
+					}
+				}
+	
+			for (int x=0;x<iArr.length;x++){
+			System.out.println(">>"+iArr[x]);
+			}
+	}		
 }
+	
